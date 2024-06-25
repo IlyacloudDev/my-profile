df63b4455a0e142423a65497340d544b0b0a3487
Ilya Stritskiy <msv_ilya@mail.ru>
2024-06-26 02:19:49 +0300

    feat: was added path to main page

--- Hobby/my_profile/urls.py
+++ Hobby/my_profile/urls.py
@@ -5,5 +5,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('about/',  TemplateView.as_view(template_name="profile/about_me.html"), name='about'),
     path('hobbies/', TemplateView.as_view(template_name="profile/hobbies.html"), name='hobbies'),
-    path('contacts/', TemplateView.as_view(template_name="profile/contacts.html"), name='contacts')
+    path('contacts/', TemplateView.as_view(template_name="profile/contacts.html"), name='contacts'),
+    path('', TemplateView.as_view(template_name="profile/main.html"), name='main'),
 ]
