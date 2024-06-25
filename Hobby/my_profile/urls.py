3d9376c9355a2b74323ce5fd9433ca4512a3487d
Ilya Stritskiy <msv_ilya@mail.ru>
2024-06-25 18:58:08 +0300

    feat: structure of URLs for changed, reason: default.html became template only for inheritance

--- Hobby/my_profile/urls.py
+++ Hobby/my_profile/urls.py
@@ -3,5 +3,7 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    path('',  TemplateView.as_view(template_name="default.html"), name='default')
+    path('about/',  TemplateView.as_view(template_name="profile/about_me.html"), name='about'),
+    path('hobbies/', TemplateView.as_view(template_name="profile/hobbies.html"), name='hobbies'),
+    path('contacts/', TemplateView.as_view(template_name="profile/contacts.html"), name='contacts')
 ]
